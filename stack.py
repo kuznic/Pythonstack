d4297d3b76eb0272f18c6da4748fe5b1fc572ff4
emeka <emeka.vinan@gmail.com>
2020-11-20 10:43:07 +0100

    .

--- stack.py
+++ stack.py
@@ -4,14 +4,14 @@ class Stack:
     def __init__(self):
         self.stack_list = []
 
-    def add_to_stack(self, item):
+    def push(self, item):
         """ Adds item to the stack"""
         self.stack_list.append(item)
 
-    def remove_from_stack(self):
+    def pop(self):
         """Takes out the item at the top of the stack"""
         self.stack_list.pop()
 
-    def print_stack(self):
+    def print(self):
         """Prints the content of the stack"""
         print(self.stack_list)
--- stack_test.py
+++ stack_test.py
@@ -5,13 +5,13 @@ def stack_test():
     """This is testing the stack.py module that is implementing a stack data
     structure as a List"""
     news_paper_stack = Stack()
-    news_paper_stack.add_to_stack("Daily Sun")
-    news_paper_stack.add_to_stack("Daily Tribune")
-    news_paper_stack.print_stack()
-    news_paper_stack.remove_from_stack()
-    news_paper_stack.print_stack()
-    news_paper_stack.add_to_stack("Daily Planet")
-    news_paper_stack.print_stack()
+    news_paper_stack.push("Daily Sun")
+    news_paper_stack.push("Daily Tribune")
+    news_paper_stack.print()
+    news_paper_stack.pop()
+    news_paper_stack.print()
+    news_paper_stack.push("Daily Planet")
+    news_paper_stack.print()
 
 
 stack_test()
